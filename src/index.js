dc6094f13195fe2cd5b2a79a1c7c354e22275499
Alexander Glassford <a.b.glassford@gmail.com>
2016-12-17 15:21:57 -0700

    fix some more initial setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-import App from './components/App';
+import { Router, Route, IndexRedirect, Redirect, browserHistory } from 'react-router';
 import './css/index.css';
+import App from './components/App';
+import Home from './components/Home';
+
 
 render(
   <Router history={browserHistory}>
-    <Route path="/" component={App}/>
+    <Route path="/" component={App}>
+      <IndexRedirect to="home"/>
+      <Route path="/home" component={Home}/>
+      <Redirect from="*" to="404"/>
+    </Route>
   </Router>,
   document.getElementById('root')
 );
