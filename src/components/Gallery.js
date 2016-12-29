4c4612309ee3ba772ff3a596f3b008e0c0311645
Alexander Glassford <a.b.glassford@gmail.com>
2016-12-29 11:26:58 -0700

    mess with gallery a bit

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,11 +7,20 @@ export default class Gallery extends Component {
     let test = [1, 2, 3, 4, 5, 6, 7]
     return (
       <div>
-        <Navbar/>
-        <h1>This is the Gallery</h1>
-        {test.map((el) => {
-          return <h1>HEY!</h1>
-        })}
+          <Navbar/>
+          <div className="container-fluid">
+            <div className="row">
+              <h1>Gallery</h1>
+              {test.map((el, i) => {
+                return (
+                <div className="col-md-4" key={i}>
+                  <h1>This is a thing</h1>
+                  <img alt="cabinet" src="http://4.bp.blogspot.com/_AZm9WkiJk20/S19VY9S3Y0I/AAAAAAAABCo/vAJFGVECicc/s1600/DSCF2391.JPG"/>
+                </div>
+                )
+              })}
+          </div>
+        </div>
       </div>
     )
   }
